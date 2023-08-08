8b8f67d211b0fa4cebe915fa38983f92a8265911
fiewza11 <137560277+fiewza11@users.noreply.github.com>
2023-08-08 17:41:25 +0700

    twocommit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,6 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-    <!-- @if ($errors->any())
-    <div id="success-alert"
-        class="alert alert-danger alert-dismissible fade show fixed-top w-25 mx-auto mt-5 text-center">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
-    @if (session('success'))
-    <div id="success-alert"
-        class="alert alert-success alert-dismissible fade show fixed-top w-25 mx-auto mt-5 text-center">
-        {{ session('success') }}
-    </div>
-    @endif
-    <script>
-        function closeAlert() {
-            document.getElementById('success-alert').style.display = 'none';
-        }
-        setTimeout(closeAlert, 3000);
-    </script> -->
     <div class="row">
         <div class="col-sm-8">
             @if (session('userdata'))
